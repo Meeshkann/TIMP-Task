67d2410740ec0a657a092ebe0132b9fb77dcfa5b
Sadomora1 <romama2024@yandex.ru>
2026-04-07 17:54:19 +0300

    echo-server draft

---
+++ MyTcpServer/mytcpserver.cpp
@@ -0,0 +1,74 @@
+#include <QDebug>
+#include "mytcpserver.h"
+#include <QCoreApplication>
+#include <QString>
+
+MyTcpServer::~MyTcpServer()
+{
+
+}
+
+MyTcpServer::MyTcpServer(QObject *parent) : QObject(parent)
+{
+    pTcpServer = new QTcpServer(this);
+
+    connect(pTcpServer, &QTcpServer::newConnection, this, &MyTcpServer::slotNewConnection);
+
+    if (!pTcpServer->listen(QHostAddress::Any, 54678))
+    {
+        qDebug() << "server is not started";
+    }
+    else
+    {
+        qDebug() << "server is started";
+    }
+}
+
+void  MyTcpServer::slotClientDisconnected()
+{
+    pTcpSocket->close();
+}
+
+void MyTcpServer::slotServerRead()
+{
+    QString res = "";
+    while(pTcpSocket->bytesAvailable()>0)
+    {
+        QByteArray array = pTcpSocket->readAll();
+
+        qDebug() << array << "\n";
+
+        if(array=="\x01")
+        {
+            pTcpSocket->write(res.toUtf8());
+            res = "";
+        }
+        else
+            res.append(array);
+    }
+    pTcpSocket->write(res.toUtf8());
+}
+
+void MyTcpServer::slotNewConnection(){
+    //   if(server_status==1){
+    pTcpSocket = pTcpServer->nextPendingConnection();
+    pTcpSocket->write("Hello, World!!! I am echo server!\r\n");
+
+    connect(pTcpSocket,&QTcpSocket::readyRead,this, &MyTcpServer::slotServerRead);
+    connect(pTcpSocket,&QTcpSocket::disconnected,this,&MyTcpServer::slotClientDisconnected);
+    // }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ MyTcpServer/mytcpserver.h
@@ -0,0 +1,24 @@
+#ifndef MYTCPSERVER_H
+#define MYTCPSERVER_H
+
+#include <QObject>
+#include <QTcpServer>
+#include <QTcpSocket>
+
+class MyTcpServer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MyTcpServer(QObject *parent = nullptr);
+    ~MyTcpServer();
+public slots:
+    void slotNewConnection();
+    void slotClientDisconnected();
+    void slotServerRead();
+private:
+    QTcpServer * pTcpServer;
+    QTcpSocket * pTcpSocket;
+    //  еще поле
+};
+
+#endif // MYTCPSERVER_H
